6f1aac2b2e33cdf654ca5f5c2eb28cb3f64bcf3e
Sebastian W. <sebastianmikael2000@gmail.com>
2025-07-14 14:34:07 +0200

    rudementary nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,34 +1,45 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { useState } from "react";
 
 export const NavBar = () => {
+	const [activeLink, setActiveLink] = useState<string>("home");
+
 	return (
 		<Navbar expand="lg" className="bg-body-tertiary">
 			<Container>
-				<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Brand href="#home">
+					<img src="logo.png" alt="Logo" />
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav">
+					<span className="navbar-toggler-icon"></span>
+				</Navbar.Toggle>
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="me-auto">
 						<Nav.Link href="#home">Home</Nav.Link>
-						<Nav.Link href="#link">Link</Nav.Link>
-						<NavDropdown title="Dropdown" id="basic-nav-dropdown">
-							<NavDropdown.Item href="#action/3.1">
-								Action
-							</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.2">
-								Another action
-							</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.3">
-								Something
-							</NavDropdown.Item>
-							<NavDropdown.Divider />
-							<NavDropdown.Item href="#action/3.4">
-								Separated link
-							</NavDropdown.Item>
-						</NavDropdown>
+						<Nav.Link href="#skills">Skills</Nav.Link>
+						<Nav.Link href="#projects">Projects</Nav.Link>
 					</Nav>
+					<span className="navbar-text">
+						<div className="social-icons">
+							<a href="#">
+								<img src="" alt="" />
+							</a>
+							<a href="#">
+								<img src="" alt="" />
+							</a>
+							<a href="#">
+								<img src="" alt="" />
+							</a>
+						</div>
+						<button
+							className="vvd"
+							onClick={() => console.log("connect")}
+						>
+							<span>Let's Connect</span>
+						</button>
+					</span>
 				</Navbar.Collapse>
 			</Container>
 		</Navbar>
